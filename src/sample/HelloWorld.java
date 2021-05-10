c1c9c65efb943ad5b1841048468ef3b73e09af0e
tejab41097@gmail.com <tejab41097@gmail.com>
2021-05-10 22:42:10 +0530

    Some another commit.

--- src/sample/HelloWorld.java
+++ src/sample/HelloWorld.java
@@ -10,18 +10,11 @@ package sample;
 public class HelloWorld {
 
 	/**
-	 * 
-	 */
-	public HelloWorld() {
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello World!!");
+		System.out.print("My new Print Statement");
 	}
 
 }
