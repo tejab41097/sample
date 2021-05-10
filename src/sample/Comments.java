d60c6efa3be87667482b0f3e177bd8244e0d4fbd
tejab41097@gmail.com <tejab41097@gmail.com>
2021-05-10 22:37:52 +0530

    First Commit

---
+++ src/sample/Comments.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class Comments {
+
+	public static void main(String[] args) {
+		// First Comment
+		/* Multi Line Comments
+		 * 
+		 * add line comment will grow
+		 */
+
+		/** Documentation Comment
+		 * 
+		 */
+	}
+
+}
---
+++ src/sample/HelloWorld.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package sample;
+
+/**
+ * @author Tej
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * 
+	 */
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!");
+	}
+
+}
